d40eb91f0d9244079f2aae82c2e697280ca68ff4
menooa25 <menooa2013@gmail.com>
2024-02-11 16:01:55 +0330

    modal is responsive with vdh and vdw

--- app/ui/Modal.tsx
+++ app/ui/Modal.tsx
@@ -10,12 +10,12 @@ const Modal = ({ id, children }: Props) => {
   return (
     <dialog
       id={id}
-      className="modal modal-bottom sm:modal-middle p-0 sm:py-1 sm:px-3 rounded-none sm:rounded-lg "
+      className="modal modal-bottom  sm:modal-middle p-0 sm:py-1 sm:px-3 rounded-none sm:rounded-lg "
     >
       <div className="modal-box ">{children}</div>
       <form
         method="dialog"
-        className="modal-backdrop w-screen h-screen sm:w-auto sm:h-auto"
+        className="modal-backdrop w-dvw h-dvh sm:w-auto sm:h-auto "
       >
         <button>close</button>
       </form>
